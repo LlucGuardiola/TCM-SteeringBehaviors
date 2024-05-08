e6a4bc2a618a22c614882b2362b542235e72fe99
LlucGuardiola <arceus88205@gmail.com>
2024-05-08 17:51:21 +0200

    w

--- Assets/Scripts/SteeringBehaviors/Behaviors/CohesionBehavior.cs
+++ Assets/Scripts/SteeringBehaviors/Behaviors/CohesionBehavior.cs
@@ -1,11 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEditor.PlayerSettings;
 
 public class CohesionBehavior : Steering
 {
+    private Transform[] targets;
+
+    void Start()
+    {
+        SteeringBehaviorController[] agents = FindObjectsOfType<SteeringBehaviorController>();
+        targets = new Transform[agents.Length - 1];
+        int count = 0;
+
+        foreach (SteeringBehaviorController agent in agents)
+        {
+            if (agent.gameObject != gameObject)
+            {
+                targets[count++] = agent.transform;
+            }
+        }
+    }
+
     public override SteeringData GetSteering(SteeringBehaviorController steeringController)
     {
-        throw new System.NotImplementedException();
+        SteeringData steering = new SteeringData();
+
+        Vector2 average = Vector2.zero;
+
+        foreach (Transform target in targets)
+        {
+            average += new Vector2(target.position.x, target.position.y);
+        }
+        average /= targets.Length;
+
+        steering.linear += average + new Vector2(transform.position.x, transform.position.y).normalized * steeringController.maxAcceleration;
+        steering.angular = 0;
+
+        return steering;
     }
 }
